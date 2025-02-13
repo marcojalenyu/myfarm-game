9e763b97de0fcf133ed88a11f6c97f7ebb223e53
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2025-02-13 22:15:40 +0800

    refac: Created empty class for Shop (to encapsulate buying seeds)

---
+++ domain/Shop.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+
+public class Shop {
+    private Map<String, Crop> cropSeeds;
+
+    // TODO: Implement a logic for buying seeds
+//    farmer.buy([seed_name], wallet) {
+//        Crop crop = cropSeeds[seed_name];
+//        if (wallet >= crop.price())
+//            wallet -= crop.price();
+//            return crop;
+//        else
+//            return null;
+//    }
+
+
+// TODO: Implement a logic for constructing the shop
+
+//    generateSeeds() {
+//        cropSeeds.put("Carrot", new Crop("Carrot", CropType.ROOT_CROP, 3, 2, 3, 2, 3, 5, 5, 10));
+//        cropSeeds.put("Tomato", new Crop("Tomato", CropType.FRUIT_TREE, 5, 3, 4, 3, 4, 10, 10, 20));
+//        cropSeeds.put("Rose", new Crop("Rose", CropType.FLOWER, 4, 2, 3, 2, 3, 5, 5, 10));
+//    }
+
+}
